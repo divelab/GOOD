77a8f7cac62fdb379590b1799e93a0423e4371f8
Citrine <citrinegui@gmail.com>
2022-06-12 12:01:27 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'autodocsumm'
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
