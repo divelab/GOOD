10ccdc299ca20425da2751f3c34d1df6f593a95e
Citrine <citrinegui@gmail.com>
2022-08-04 17:09:34 -0500

    Debug: GOODPCBA-DIR [skip ci]

--- GOOD/ood_algorithms/algorithms/DIR.py
+++ GOOD/ood_algorithms/algorithms/DIR.py
@@ -20,14 +20,33 @@ class DIR(BaseOODAlg):
 
     def __init__(self, config: Union[CommonArgs, Munch]):
         super(DIR, self).__init__(config)
-
-
+        self.rep_out = None
+        self.causal_out = None
+        self.conf_out = None
 
     def stage_control(self, config: Union[CommonArgs, Munch]):
         if self.stage == 0 and at_stage(1, config):
             reset_random_seed(config)
             self.stage = 1
         config.train.alpha = config.ood.extra_param[0] * (config.train.epoch ** 1.6)
+
+    def output_postprocess(self, model_output: Tensor, **kwargs) -> Tensor:
+        r"""
+        Process the raw output of model
+
+        Args:
+            model_output (Tensor): model raw output
+
+        Returns (Tensor):
+            model raw predictions
+
+        """
+        if isinstance(model_output, tuple):
+            self.rep_out, self.causal_out, self.conf_out = model_output
+        else:
+            self.causal_out = model_output
+            self.rep_out, self.conf_out = None, None
+        return self.causal_out
 
     def loss_calculate(self, raw_pred: Tensor, targets: Tensor, mask: Tensor, node_norm: Tensor,
                        config: Union[CommonArgs, Munch]) -> Tensor:
@@ -52,24 +71,23 @@ class DIR(BaseOODAlg):
             loss based on IRM algorithm
 
         """
-        if isinstance(raw_pred, tuple):
-            rep_out, causal_out, conf_out = raw_pred
-            causal_loss = (config.metric.loss_func(causal_out, targets, reduction='none') * mask).sum() / mask.sum()
-            conf_loss = (config.metric.loss_func(conf_out, targets, reduction='none') * mask).sum() / mask.sum()
+
+        if self.rep_out is not None:
+            causal_loss = (config.metric.loss_func(raw_pred, targets, reduction='none') * mask).sum() / mask.sum()
+            conf_loss = (config.metric.loss_func(self.conf_out, targets, reduction='none') * mask).sum() / mask.sum()
 
             env_loss = torch.tensor([]).to(config.device)
-            for rep in rep_out:
+            for rep in self.rep_out:
                 tmp = (config.metric.loss_func(rep, targets, reduction='none') * mask).sum() / mask.sum()
                 env_loss = torch.cat([env_loss, (tmp.sum() / mask.sum()).unsqueeze(0)])
             causal_loss += config.train.alpha * env_loss.mean()
-            env_loss = config.train.alpha * torch.var(env_loss * rep_out.size(0))
+            env_loss = config.train.alpha * torch.var(env_loss * self.rep_out.size(0))
 
             loss = causal_loss + env_loss + conf_loss
             self.mean_loss = causal_loss
             self.spec_loss = env_loss + conf_loss
         else:
-            causal_out = raw_pred
-            causal_loss = (config.metric.loss_func(causal_out, targets, reduction='none') * mask).sum() / mask.sum()
+            causal_loss = (config.metric.loss_func(raw_pred, targets, reduction='none') * mask).sum() / mask.sum()
 
             loss = causal_loss
             self.mean_loss = causal_loss
